7df30f7094122a2cfe3bf8c4e1ff487b6a8f3cf1
Светлана <sv.egorova6495@mail.ru>
2026-03-23 21:48:40 +0400

    final dz models admin

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True)
+
+    skills = models.ManyToManyField(Skill)
+    skill_level = models.IntegerField()
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
--- workplaces/models.py
+++ workplaces/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from employees.models import Employee
 
-# Create your models here.
+class Workplace(models.Model):
+    desk_number = models.IntegerField()
+    info = models.TextField(blank=True)
+
+    employee = models.OneToOneField(Employee, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Стол {self.desk_number}"
